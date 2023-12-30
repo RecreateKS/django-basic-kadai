87324f082286b37adffce15cc56eb453ad3fd188
RecreateKS <148827652+RecreateKS@users.noreply.github.com>
2023-12-30 23:18:18 +0900

    Add files via upload

---
+++ kadai_010/crud/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 3
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
